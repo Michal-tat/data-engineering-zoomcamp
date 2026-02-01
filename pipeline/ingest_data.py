4be002a0a58fa2e0eda2bf3317de527318f7f8f4
horaldo <56773789+Michal-tat@users.noreply.github.com>
2026-02-01 18:25:32 +0000

    added docker-compose

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,70 +1,59 @@
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
 
-def run():
-    # 1. Parametry połączenia i pliku
-    user = 'root'
-    password = 'root'
-    host = 'localhost'
-    port = 5432
-    db = 'ny_taxi'
-    table_name = 'yellow_taxi_data'
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
     
-    year = 2021
-    month = "01" # jako string, żeby zachować zero w "01"
+    # Formatujemy miesiąc, aby zawsze miał dwie cyfry (np. 1 -> "01", 10 -> "10")
+    month_str = f"{month:02d}"
     
-    # Adres URL z użyciem f-string (zwróć uwagę na 'f' przed cudzysłowem)
-    url = f'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_{year}-{month}.csv.gz'
+    # Adres URL z użyciem parametrów
+    url = f'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_{year}-{month_str}.csv.gz'
 
-    # 2. Definicja typów danych
+    # Definicja typów danych
     dtype = {
-        "VendorID": "Int64",
-        "passenger_count": "Int64",
-        "trip_distance": "float64",
-        "RatecodeID": "Int64",
-        "store_and_fwd_flag": "string",
-        "PULocationID": "Int64",
-        "DOLocationID": "Int64",
-        "payment_type": "Int64",
-        "fare_amount": "float64",
-        "extra": "float64",
-        "mta_tax": "float64",
-        "tip_amount": "float64",
-        "tolls_amount": "float64",
-        "improvement_surcharge": "float64",
-        "total_amount": "float64",
-        "congestion_surcharge": "float64"
+        "VendorID": "Int64", "passenger_count": "Int64", "trip_distance": "float64",
+        "RatecodeID": "Int64", "store_and_fwd_flag": "string", "PULocationID": "Int64",
+        "DOLocationID": "Int64", "payment_type": "Int64", "fare_amount": "float64",
+        "extra": "float64", "mta_tax": "float64", "tip_amount": "float64",
+        "tolls_amount": "float64", "improvement_surcharge": "float64",
+        "total_amount": "float64", "congestion_surcharge": "float64"
     }
     parse_dates = ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
 
-    # 3. Tworzenie połączenia z bazą
-    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    # 3. Tworzenie połączenia - używamy zmiennych z argumentów funkcji!
+    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
-    # 4. Przygotowanie iteratora (wczytujemy paczkami po 100 000 wierszy)
+    # 4. Przygotowanie iteratora
     df_iter = pd.read_csv(
         url,
         dtype=dtype,
         parse_dates=parse_dates,
         iterator=True,
-        chunksize=100000
+        chunksize=chunksize
     )
 
-    # 5. Pobieramy pierwszą paczkę, aby stworzyć strukturę tabeli
+    # 5. Pierwsza paczka i schemat
     df = next(df_iter)
-    
-    # 'replace' usunie tabelę jeśli istniała i stworzy nową z samym nagłówkiem
-    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+    df.head(n=0).to_sql(name=target_table, con=engine, if_exists='replace')
 
-    # 6. Pętla ładująca dane
-    print(f"Rozpoczynam ładowanie danych do tabeli {table_name}...")
-    
-    # Ładujemy pierwszą paczkę (tę już pobraną)
-    df.to_sql(name=table_name, con=engine, if_exists='append')
+    # 6. Pętla ładująca
+    print(f"Ładowanie danych do tabeli {target_table} na host {pg_host}...")
+    df.to_sql(name=target_table, con=engine, if_exists='append')
 
-    # Ładujemy resztę w pętli
     for df_chunk in tqdm(df_iter):
-        df_chunk.to_sql(name=table_name, con=engine, if_exists='append')
+        df_chunk.to_sql(name=target_table, con=engine, if_exists='append')
     
     print("Zakończono sukcesem!")
 
